a66009badb97762634c32a3d853914849c04c687
Pavel Sidorov <52633235+sxwiix@users.noreply.github.com>
2024-10-02 18:54:24 +0300

    fix redoc

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -10,7 +10,9 @@ schema_view = get_schema_view(
     openapi.Info(
         title="Kitten API",
         default_version='v1',
-        description="API для онлайн выставки котят",
+        description="Documentation `ReDoc` view can be found [here](/redoc).",
+        contact=openapi.Contact(email="pavlosidorov@mail.ru"),
+        license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
@@ -20,6 +22,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     # Эндпоинты для JWT аутентификации
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
